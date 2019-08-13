aff9aeed4ab1d43f0cd98213cde209fe0f0a827f
Вася Пупкин <BomBimBom@bk.ru>
2019-08-13 17:50:29 +0300

    делаю закхват с камеры

--- RussianСertification/Controllers/HomeController.cs
+++ RussianСertification/Controllers/HomeController.cs
@@ -39,6 +39,10 @@ namespace RussianСertification.Controllers
         {
             return View();
         }
+        public IActionResult Testing()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
